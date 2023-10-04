3b7c35bd7ea233c56b12cd9f16d2894afb9ff0c9
Dhiali <231299@virtualwindow.co.za>
2023-10-04 22:37:23 +0200

    final trm 3

--- scripts/main.js
+++ scripts/main.js
@@ -1,197 +1,300 @@
-//creating an array of objects
+const tripsEl = document.querySelector(".trips");
+const cartTicketsEl = document.querySelector(".cart-tickets");
+const subtotalEl = document.querySelector(".subtotal");
+const totalTicketsInCartEl = document.querySelector(".total-tickets-in-cart");
 
-const arrPlants = [
 
-    {"name": "Fikus Tree",
-    "price": 350,
-    "description": "Graceful and lush, this charming indoor plant boasts glossy, emerald-green leaves that effortlessly brighten any space.",
-    "image": "plant1.png",
-    "lightAmount" : "low",
-    "addedDate":"2023-02-5",
-    "onSale" : "true",
-    
-  },
-    
+function renderTrips() {
+  trips.forEach((trip) => {
+    tripsEl.innerHTML += `
+            <div class="ticket">
+                <div class="ticket-container">
+                    <div class="ticket-img">
+                        <img src="${trip.imgSrc}" alt="${trip.name}">
+                    </div>
+                    <div class="desc">
+                        <h4>${trip.name}</h4>
+                        <h5><small>R</small>${trip.price}</h5>
+                        <p>
+                            ${trip.description}
+                        </p>
+                    </div>
+                    
+                    <div class="add-to-cart" onclick="addToCart(${trip.id})">
+                        <img src="assets/shopping-bag-svgrepo-com (1).svg" alt="add to cart">
+                    </div>
+                </div>
+            </div>
+        `;
+  });
+}
+renderTrips();
+
+
+
+
+let cart = JSON.parse(localStorage.getItem("CART")) || [];
+updateCart();
+
+function addToCart(id) { 
   
-  {
-    "name": "White Sprite Succulent",
-    "price": 200,
-    "description": "Delicate and captivating, this rare succulent showcases a mesmerizing silver-white hue that gracefully adorns its petite, fleshy leaves.",
-    "image": "plant2.png",
-    "lightAmount" : "high",
-    "addedDate":"2023-04-2",
-    "onSale" : "false",
-  },
-  {
-    "name": "Snake Plant",
-    "price": 400,
-    "description": "Boasting tall, sleek, and sword-like leaves, this botanical marvel adds a touch of modern flair to any setting.",
-    "image": "plant3.png",
-    "lightAmount" : "low",
-    "addedDate":"2023-04-5",
-    "onSale" : "true",
-  },
-  {
-    "name": "Parlour Palm",
-    "price": 350,
-    "description": "With its lush, feather-like fronds and compact size, this indoor beauty makes a striking addition to any interior space.",
-    "image": "plant4.png",
-    "lightAmount" : "low",
-    "addedDate":"2023-03-25",
-    "onSale" : "false",
-  },
-  {
-    "name": "Japanese Maple",
-    "price": 1200,
-    "description": "Known for its stunning foliage that transforms with the seasons, this ornamental tree captivates with its delicate, lacy leaves in vibrant shades of red, orange, or gold.",
-    "image": "plant5.png",
-    "lightAmount" : "bright",
-    "addedDate":"2023-09-2",
-    "onSale" : "true",
+  if (cart.some((ticket) => ticket.id === id)) {
+    changeNumberOfUnits("plus", id);
+  } else { 
+    const ticket = trips.find((trip) => trip.id === id);
+
+    cart.push({
+      ...ticket,
+      numberOfUnits: 1,
+    
+    });
   }
+
+  updateCart();
+}
+
+
+
+
+
+
+function updateCart() {
+  renderCartTickets();
+  renderSubtotal();
+
+ 
+  localStorage.setItem("CART", JSON.stringify(cart))
+}
+
+
+function renderSubtotal() {
+  let totalPrice = 0,
+    totalTickets = 0;
+
+  cart.forEach((ticket) => {
+    totalPrice += ticket.price * ticket.numberOfUnits;
+    totalTickets += ticket.numberOfUnits;
+  });
+
+  subtotalEl.innerHTML = `Subtotal (${totalTickets} tickets): R${totalPrice.toFixed(2)}`;
+  totalTicketsInCartEl.innerHTML = totalTickets;
+}
+
+
+
+
+function renderCartTickets() {
+  cartTicketsEl.innerHTML = ""; 
+  cart.forEach((ticket) => {
+    cartTicketsEl.innerHTML += `
+        <div class="cart-ticket">
+            <div class="ticket-info" onclick="removeTicketFromCart(${ticket.id})">
+                <img src="${ticket.imgSrc}" alt="${ticket.name}">
+                <h4>${ticket.name}</h4>
+            </div>
+            <div class="unit-price">
+                <small>R</small>${ticket.price}
+            </div>
+            <div class="units">
+                <div class="btn minus" onclick="changeNumberOfUnits('minus', ${ticket.id})">-</div>
+                <div class="number">${ticket.numberOfUnits}</div>
+                <div class="btn plus" onclick="changeNumberOfUnits('plus', ${ticket.id})">+</div>           
+            </div>
+        </div>
+      `;
+  });
+}
+
+
+
+
+function removeTicketFromCart(id) {
+  cart = cart.filter((ticket) => ticket.id !== id);
+
+  updateCart();
+}
+
+
+
+
+function changeNumberOfUnits(action, id) {
+  cart = cart.map((ticket) => {
+    let numberOfUnits = ticket.numberOfUnits;
+
+    if (ticket.id === id) {
+      if (action === "minus" && numberOfUnits > 1) {
+        numberOfUnits--;
+      } else if (action === "plus" && numberOfUnits < ticket.instock) {
+        numberOfUnits++;
+      }
+    };
+
+    return {
+      ...ticket,
+      numberOfUnits,
+    };
+  });
+
+  updateCart();
+}
+
+
+
+
+function showAllTrips() {
+  renderTrips(); 
+}
+
+document.getElementById("filterAllButton").addEventListener("click", showAllTrips);
+
+
+function filterBy5DaysOrLess() {
+  const filteredTrips = trips.filter((trip) => {
+   
+    const days = parseInt(trip.name.match(/\d+/)[0]);
+
     
-];
+    return days <= 5;
+  });
+
+ 
+  renderTrips(filteredTrips);
+}
 
 
-const onSale = true;
+document.getElementById("filterButton").addEventListener("click", filterBy5DaysOrLess);
 
 
-const productsWithOnSale = arrPlants.map(arrPlants => {
-    return { ...arrPlants, onSale: onSale };
+
+
+
+function filterMoreThan5Days() {
+  const filteredTrips = trips.filter((trip) => {
+   
+    const days = parseInt(trip.name.match(/\d+/)[0]);
+
+   
+    return days > 5;
   });
 
   
-  console.log(productsWithOnSale);
-
-let appliedFilter = "";
-let appliedSort = "date added";;
+  renderTrips(filteredTrips);
+}
 
 
+document.getElementById("filterMoreThan5DaysButton").addEventListener("click", filterMoreThan5Days);
 
-$(document).ready(function(){
-//select the plants container and add html from our template 
+
+
+function filterOneLocation() {
+  const filteredTrips = trips.filter((trip) => {
     
-
-//you have to call it only when ready/loaded
-    filterSortPlants();
-});
-
-
-
-function loadPlants (plantsToShow) {
-
-  console.log(plantsToShow);
-$("#plantsContainer").empty();
-
-
-
-    for (let i = 0; i < plantsToShow.length; i++) {
-        const plant = plantsToShow[i];
-
-
-        console.log(plant.name);
-
-        $("#plantsContainer").append($("#plantCardTemplate").html());
-
-        let currentChild = $("#plantsContainer").children().eq(i);
-
-        $(currentChild).find(".card-img-top").attr('src','assets/' + arrPlants[i].image);
-
-        $(currentChild).find("#nameText").text(arrPlants[i].name);
-
-        $(currentChild).find("#priceText").text('R ' + arrPlants[i].price);
-
-        $(currentChild).find("#descriptionText").text(arrPlants[i].description);
-
-        $(currentChild).find("#descriptionText").hide();
-        
-        
-    };
-}
-
-
-
-$("input[name='filterRadio']").click(function() {
-
-  appliedFilter = $(this).attr('value');
-
-  console.log(appliedFilter);
-  filterSortPlants();
-});
-
-$("input[name='sortRadio']").click(function() {
-
-  appliedSort = $(this).attr('value');
-
-  console.log(appliedFilter);
-});
-
-function filterSortPlants (){
-
- let filteredSortedArrPlants = [];
-
-if(appliedFilter){
-  filteredSortedArrPlants = arrPlants.filter(arrPlants => arrPlants.lightAmount == appliedFilter);
-}
-else{
-  filteredSortedArrPlants = arrPlants;
-};
-
-
- if(appliedSort == "low to high"){
-    filteredSortedArrPlants = filteredSortedArrPlants.sort((a,b) => {
-      return a.price - b.price;
-    });
- }
- else if (appliedSort === "data added") {
-  filteredSortedArrPlants = filteredSortedArrPlants.sort((a,b) => {
-        let da = new Date(a.addedDate);
-        let db = new Date(b.addedDate);
-
-        return db-da;
+    const descriptionWords = trip.description.split(' ');
+    return descriptionWords.length === 1;
   });
 
-  if(appliedSort == "ascending letters"){
-    filteredSortedArrPlants = filteredSortedArrPlants.sort((a,b) => {
-      return a.price - b.price;
-    });
- }
- else{
-  filteredSortedArrPlants = arrPlants;
-};
 
-
-
- }
- 
- 
- loadPlants(filteredSortedArrPlants);
-
-
+  renderTrips(filteredTrips);
 }
 
 
-$("#plantsContainer").on('click', '.card', function(){
-
-  // Toggle the price & description text
-  $(this).find("#priceText").toggle();
-  $(this).find("#descriptionText").toggle();
-
-  // Resize the image to fit the additional content
-  $(this).find(".card-img-top").toggleClass("small");
-
-});
+document.getElementById("filterOneLocationButton").addEventListener("click", filterOneLocation);
 
 
+function filterCheapestTrips() {
+  
+  const sortedTrips = trips.slice().sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
 
-$(document).ready(function() {
-  // Binds the click event to the "Remove" button
-  $('.remove-btn').on('click', function() {
-    
-    var row = $(this).parent().parent(); // cross two levels up to the parent <tr>
+  
+  const cheapestTrips = sortedTrips.slice(0, 5);
 
-    // Remove the row from the table
-    row.remove();
+  
+  renderTrips(cheapestTrips);
+}
+
+
+document.getElementById("filterCheapestTripsButton").addEventListener("click", filterCheapestTrips);
+
+
+
+function filterMultipleLocations() {
+  const filteredTrips = trips.filter((trip) => {
+    const descriptionWords = trip.description.split(' ');
+    return descriptionWords.length > 1;
   });
+
+  
+  renderTrips(filteredTrips);
+}
+
+
+document.getElementById("filterMultipleLocationsButton").addEventListener("click", filterMultipleLocations);
+
+
+function filterReturnToPort() {
+  const returnToPortTrips = trips.filter((trip) => {
+    return trip.description.toLowerCase().includes("return to port");
+  });
+
+
+  renderTrips(returnToPortTrips);
+}
+
+
+document.getElementById("filterReturnToPortButton").addEventListener("click", filterReturnToPort);
+
+
+
+function renderTrips(tripsToRender) {
+  const tripsContainer = document.querySelector(".trips");
+  tripsContainer.innerHTML = ""; 
+
+
+  const tripsToDisplay = tripsToRender || trips;
+
+  tripsToDisplay.forEach((trip) => {
+    
+
+    tripsEl.innerHTML += `
+    <div class="ticket">
+        <div class="ticket-container">
+            <div class="ticket-img">
+                <img src="${trip.imgSrc}" alt="${trip.name}">
+            </div>
+            <div class="desc">
+                <h4>${trip.name}</h4>
+                <h5><small>R</small>${trip.price}</h5>
+                <p>
+                    ${trip.description}
+                </p>
+            </div>
+            
+            <div class="add-to-cart" onclick="addToCart(${trip.id})">
+                <img src="assets/shopping-bag-svgrepo-com (1).svg" alt="add to cart">
+            </div>
+        </div>
+    </div>
+`;
+
+
+
+
+    
+  });
+}
+
+
+renderTrips();
+
+
+const successMessage = document.getElementById("success-message");
+const checkoutButton = document.querySelector(".checkout");
+
+
+checkoutButton.addEventListener("click", function() {
+ successMessage.style.display = "block"; 
+  setTimeout(function() {
+    successMessage.style.display = "none"; 
+  }, 3000); 
 });
 
